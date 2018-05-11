dae06c618aa48d14a66fba8052f28382cfbaaa05
Allan Carlos Ramalho <allan.c.ramalho@gmail.com>
2018-05-11 05:38:25 -0300

    Reimplementação de PoC de processamento OCR. Os primeiros commits não contemplavam as funcionalidades, por engano durante fase de desenvolvimento.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,107 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ActionSheetController, LoadingController } from 'ionic-angular';
+import { Camera } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  srcImage: string;
+  OCRAD: any;
+  Tesseract: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public actionSheetCtrl: ActionSheetController,
+    public loadingCtrl: LoadingController,
+    private camera: Camera
+  ) {}
 
+  presentActionSheet() {
+    const actionSheet = this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Escolher imagem',
+          handler: () => {
+            this.getPicture(0); // 0 == Library
+          }
+        },{
+          text: 'Fotografar',
+          handler: () => {
+            this.getPicture(1); // 1 == Camera
+          }
+        },{
+          text: 'Demonstração - Texto Limpo',
+          handler: () => {
+            this.srcImage = 'assets/imgs/demo1.png';
+          }
+        },{
+          text: 'Demonstração - RG Cor',
+          handler: () => {
+            this.srcImage = 'assets/imgs/demo2.png';
+          }
+        },{
+          text: 'Demonstração - RG P&B',
+          handler: () => {
+            this.srcImage = 'assets/imgs/demo2.png';
+          }
+        },{
+          text: 'Cancelar',
+          role: 'cancel'
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  getPicture(sourceType: number) {
+    this.camera.getPicture 	({
+      quality: 100,
+      destinationType: 0,
+      sourceType,
+      allowEdit: true,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    }).then((imageData) => {
+      this.srcImage = `data:image/jpeg;base64,${imageData}`;
+    }, (err) => {
+      console.log(`ERROR -> ${JSON.stringify(err)}`);
+    });
+  }
+
+  // Fonte: https://github.com/allancr/dispenser-poc-ocr
+  ocradjs() {
+    let loader = this.loadingCtrl.create({
+     content: 'Processando imagem com OCRAD JS...'
+    });
+    loader.present();
+    OCRAD(document.getElementById('image'), text => {
+      loader.dismissAll();
+      console.log(text);
+      alert(text);
+    });
+  }
+
+  // Fonte: https://github.com/naptha/tesseract.js#tesseractjs
+  tesseractjs() {
+    let loader = this.loadingCtrl.create({
+     content: 'Processando imagem com Tesseract JS...'
+    });
+    loader.present();
+
+    Tesseract.recognize(document.getElementById('image'))
+	.then(function(result){
+	    console.log(result)
+	    loader.dismissAll();
+	    console.log(result);
+	    alert(result.text);
+	});
+  }
+
+  restart() {
+    this.srcImage = '';
+    this.presentActionSheet();
   }
 
 }
